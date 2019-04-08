766b6d030210d72f4a9a344b0d4f50a7adf9aed2
kutomore <kutomore@gmail.com>
2019-04-08 10:09:38 -0300

    Improving general writing of code

--- spec/lerolero_generator/speech_spec.rb
+++ spec/lerolero_generator/speech_spec.rb
@@ -1,47 +1,46 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe "Speech" do
-
-  it ".sentence" do
+describe 'Speech' do
+  it '.sentence' do
     sentence = Speech.sentence
     expect(sentence).to be_a_kind_of(String)
   end
 
-  it ".sentence(5)" do
+  it '.sentence(5)' do
     quantity = 5
     sentence = Speech.sentence(quantity)
     expect(sentence).to be_a_kind_of(String)
-    expect(sentence.count(".")).to eql(quantity)
+    expect(sentence.count('.')).to eql(quantity)
   end
 
-  it ".paragraph" do
+  it '.paragraph' do
     paragraph = Speech.paragraph
     expect(paragraph).to be_a_kind_of(String)
   end
 
-  it ".paragraph(5)" do
+  it '.paragraph(5)' do
     quantity = 5
     paragraph = Speech.paragraph(quantity)
     expect(paragraph).to be_a_kind_of(String)
-    expect(paragraph.count("\n")).to eql((quantity-1)*2)
+    expect(paragraph.count("\n")).to eql((quantity - 1) * 2)
   end
 
-  it ".first_talk" do
+  it '.first_talk' do
     first_talk = Speech.first_talk
     expect(first_talk).to be_a_kind_of(String)
   end
 
-  it ".second_talk" do
+  it '.second_talk' do
     second_talk = Speech.second_talk
     expect(second_talk).to be_a_kind_of(String)
   end
 
-  it ".third_talk" do
+  it '.third_talk' do
     third_talk = Speech.third_talk
     expect(third_talk).to be_a_kind_of(String)
   end
 
-  it ".fourth_talk" do
+  it '.fourth_talk' do
     fourth_talk = Speech.fourth_talk
     expect(fourth_talk).to be_a_kind_of(String)
   end
